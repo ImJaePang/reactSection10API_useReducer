86e8386b012b447cc8a63806c4b31b2b5f651e6c
ImJaePang <narushel@naver.com>
2025-02-09 22:53:38 +0900

    useContextPractice

--- src/components/CartModal.jsx
+++ src/components/CartModal.jsx
@@ -1,11 +1,16 @@
+import { useContext } from 'react';
 import { forwardRef, useImperativeHandle, useRef } from 'react';
 import { createPortal } from 'react-dom';
 import Cart from './Cart';
+import { CartContext } from '../store/shopping-cart-context.jsx';
 
 const CartModal = forwardRef(function Modal(
-  { cartItems, onUpdateCartItemQuantity, title, actions },
+  { title, actions },
   ref
 ) {
+
+  const {items}= useContext(CartContext);
+
   const dialog = useRef();
 
   useImperativeHandle(ref, () => {
@@ -19,7 +24,7 @@ const CartModal = forwardRef(function Modal(
   return createPortal(
     <dialog id="modal" ref={dialog}>
       <h2>{title}</h2>
-      <Cart items={cartItems} onUpdateItemQuantity={onUpdateCartItemQuantity} />
+      <Cart items={items}  />
       <form method="dialog" id="modal-actions">
         {actions}
       </form>
